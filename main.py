479d4272c1d354663c8f29b0ea2ebb37d5453809
Tushar Saini <tusharsaini1854@gmail.com>
2023-07-07 17:42:40 +0530

    add import os

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 from flask_gravatar import Gravatar
-
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -258,7 +258,7 @@ def delete_post(post_id):
 #     db.session.commit()
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+# if __name__ == "__main__":
+#     app.run(debug=True)
